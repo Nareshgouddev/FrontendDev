9377a958ba20acf9e4213709be17848a1167cad4
Naresh goud <ediganareshgoud406@gmail.com>
2026-01-09 21:42:22 +0530

    js

--- PIXORA/Script.js
+++ PIXORA/Script.js
@@ -1,2 +1,3 @@
-
-
+const second_page = document.getElementById("second_page");
+const right_cards = document.getElementsByClassName("second_page-right_cards");
+second_page.scrollIntoView(right_cards);
